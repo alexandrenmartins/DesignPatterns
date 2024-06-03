2cc00993d4dc8b0a74729c109c316e4892b79340
Alexandre Nunes <alexandrenunesmartins@hotmail.com>
2024-06-03 10:20:37 -0300

    Alteracao query Pool

--- src/criacionais/singleton/connectionPool/before/Client.java
+++ src/criacionais/singleton/connectionPool/before/Client.java
@@ -7,29 +7,29 @@ public class Client {
 	public static void doQuery1() {
 		ConnectionPool pool = new ConnectionPool();
 		Connection conn = pool.getConnection();
-		if(conn != null)
-			conn.query("SELECT * FROM A1");
+		if (conn != null)
+			conn.query("SELECT * FROM P1");
 	}
-	
+
 	public static void doQuery2() {
 		ConnectionPool pool = new ConnectionPool();
 		Connection conn = pool.getConnection();
-		if(conn != null)
-			conn.query("SELECT * FROM A2");
+		if (conn != null)
+			conn.query("SELECT * FROM P2");
 	}
-	
+
 	public static void doQuery3() {
 		ConnectionPool pool = new ConnectionPool();
 		Connection conn = pool.getConnection();
-		if(conn != null)
-			conn.query("SELECT * FROM A3");
+		if (conn != null)
+			conn.query("SELECT * FROM P3");
 	}
 
 	public static void main(String[] args) {
 		doQuery1();
 		doQuery2();
 		doQuery3();
-		
-		//3 connections was made, even with 2 connections limit
+
+		// 3 connections was made, even with 2 connections limit
 	}
 }
