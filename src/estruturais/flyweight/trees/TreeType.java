2d94d4e5a9092bdf400b78ec800df1473c6cffec
Alexandre Nunes <alexandrenunesmartins@hotmail.com>
2024-06-05 16:02:54 -0300

    Estruturais Flyweight

---
+++ src/estruturais/flyweight/trees/TreeType.java
@@ -0,0 +1,24 @@
+package estruturais.flyweight.trees;
+
+import java.awt.*;
+
+public class TreeType {
+    @SuppressWarnings("unused")
+    private String name;
+    private Color color;
+    @SuppressWarnings("unused")
+    private String otherTreeData;
+
+    public TreeType(String name, Color color, String otherTreeData) {
+        this.name = name;
+        this.color = color;
+        this.otherTreeData = otherTreeData;
+    }
+
+    public void draw(Graphics g, int x, int y) {
+        g.setColor(Color.BLACK);
+        g.fillRect(x - 1, y, 3, 5);
+        g.setColor(color);
+        g.fillOval(x - 5, y - 10, 10, 10);
+    }
+}
