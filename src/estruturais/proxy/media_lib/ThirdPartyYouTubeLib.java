bc73aef95cb181da46e056ec8963d8ab1afbbfc3
Alexandre Nunes <alexandrenunesmartins@hotmail.com>
2024-06-05 16:52:52 -0300

    Estruturais Proxy

---
+++ src/estruturais/proxy/media_lib/ThirdPartyYouTubeLib.java
@@ -0,0 +1,9 @@
+package estruturais.proxy.media_lib;
+
+import java.util.HashMap;
+
+public interface ThirdPartyYouTubeLib {
+    HashMap<String, Video> popularVideos();
+
+    Video getVideo(String videoId);
+}
